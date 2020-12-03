d010c977a83ba98570102ead862c9c6e677e3aa7
starwort <tcphone93@gmail.com>
2020-12-03 05:13:24 +0000

    Add day 3

--- day_03.py
+++ day_03.py
@@ -1,18 +1,47 @@
 import aoc_helper
 
 raw = aoc_helper.day(3)
-print(raw)
+# print(raw)
+
 
 def parse_raw():
-    ...
+    return [[char == "#" for char in i] for i in raw.splitlines()]
+
 
 data = parse_raw()
 
+
 def part_one():
-    ...
+    x, y = (0, 0)
+    trees = 0
+    while y < len(data):
+        if data[y][x % len(data[0])]:
+            trees += 1
+        x += 3
+        y += 1
+    return trees
+
+
+def one_slope(dx, dy):
+    x, y = (0, 0)
+    trees = 0
+    while y < len(data):
+        if data[y][x % len(data[0])]:
+            trees += 1
+        x += dx
+        y += dy
+    return trees
+
 
 def part_two():
-    ...
+    return (
+        one_slope(1, 1)
+        * one_slope(3, 1)
+        * one_slope(5, 1)
+        * one_slope(7, 1)
+        * one_slope(1, 2)
+    )
+
 
 aoc_helper.submit(day=3, solution=part_one)
 aoc_helper.submit(day=3, solution=part_two)
