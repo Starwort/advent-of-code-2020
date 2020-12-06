a0e051f8988d405d524d61bbd6610e0dff4881c3
starwort <tcphone93@gmail.com>
2020-12-06 05:14:40 +0000

    Add both spaghetti competiton code and cleaned up code for day 6

--- day_06.py
+++ day_06.py
@@ -1,18 +1,37 @@
+import string
+
 import aoc_helper
 
 raw = aoc_helper.day(6)
-print(raw)
+# print(raw)
+
 
 def parse_raw():
-    ...
+    parts = raw.split("\n\n")
+    return [set(i) - {"\n"} for i in parts]
+
+
+def parse_raw_2():
+    parts = raw.split("\n\n")
+    rv = []
+    for part in parts:
+        out = set(string.ascii_lowercase)
+        for line in part.split("\n"):
+            out &= set(line)
+        rv.append(out)
+    return rv
+
 
 data = parse_raw()
 
+
 def part_one():
-    ...
+    return sum(map(len, data))
+
 
 def part_two():
-    ...
+    return sum(map(len, parse_raw_2()))
+
 
 aoc_helper.submit(day=6, solution=part_one)
 aoc_helper.submit(day=6, solution=part_two)
---
+++ day_06_comp.py
@@ -0,0 +1,26 @@
+import string
+
+import aoc_helper
+
+raw = aoc_helper.day(6)
+# print(raw)
+
+
+def parse_raw():
+    parts = raw.split("\n\n")
+    return [[set(i) for i in part.splitlines()] for part in parts]
+
+
+data = parse_raw()
+
+
+def part_one():
+    return sum(map(len, map(lambda i: set.union(*i), data)))
+
+
+def part_two():
+    return sum(map(len, map(lambda i: set.intersection(*i), data)))
+
+
+aoc_helper.submit(day=6, solution=part_one)
+aoc_helper.submit(day=6, solution=part_two)
