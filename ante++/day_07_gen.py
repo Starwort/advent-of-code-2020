a266b905e00bf62bc6d60f7cf9d8d7d7949300d8
starwort <tcphone93@gmail.com>
2020-12-08 11:30:06 +0000

    Split day_07 into real code and ante++ code, add colours and modifiers, generator beginning

--- ante++/day_07_gen.py
+++ ante++/day_07_gen.py
@@ -1,2 +1,34 @@
 # be sure to count all of the bags, even if the nesting becomes topologically impractical!
 # :)
+
+import pathlib
+import random
+
+MODIFIERS = set(
+    (pathlib.Path(__file__).parent / "modifiers.txt").read_text().splitlines()
+)
+COLOURS = set((pathlib.Path(__file__).parent / "colours.txt").read_text().splitlines())
+ALL_BAGS = [modifier + " " + colour for modifier in MODIFIERS for colour in COLOURS]
+for _ in range(7):
+    random.shuffle(ALL_BAGS)
+TOTAL_BAGS = len(ALL_BAGS)
+
+
+def format_rule(rule: tuple[str, list[tuple[float, str]]]) -> str:
+    name, contains = rule
+    if not contains:
+        return f"{name} bags contain no other bags."
+
+
+def random_amount() -> float:
+    ipart = random.randrange(-5000, 5000)
+    fpart = random.random()
+    return ipart + fpart
+
+
+def generate_input():
+    used_bags = set()
+    rules = {}
+    for bag in ALL_BAGS:
+        contains = random.randint(0, len(used_bags))
+        rules[bag] = [(random_amount(), i) for i in random.sample(used_bags, contains)]
--- ante++/day_07_solver.py
+++ ante++/day_07_solver.py
@@ -1,13 +1,12 @@
+import pathlib
 import re
 from collections import defaultdict
 
-import aoc_helper
-
-raw = aoc_helper.fetch(7)
+raw = (pathlib.Path(__file__).parent / "07.in").read_text()
 # print(raw)
 
 CONTAINS = re.compile(r"(.*?) bags contain (.*?)\.")
-BAG = re.compile(r"(\d+) (.*?) bags?")
+BAG = re.compile(r"((-|\+)?\d+(\.\d+)?) (.*?) bags?")
 NOT_CONTAIN = re.compile(r"(.*?) bags contain no other bags.")
 
 
@@ -16,7 +15,7 @@ def parse_raw():
     inverse_rules = defaultdict(list)
     for bag, contains, *_ in CONTAINS.findall(raw):
         bags = BAG.findall(contains)
-        rules[bag] = [(int(bag_[0]), bag_[1]) for bag_ in bags]
+        rules[bag] = [(float(bag_[0]), bag_[1]) for bag_ in bags]
         for _, name in bags:
             inverse_rules[name].append(bag)
     for bag in NOT_CONTAIN.findall(raw):
@@ -41,12 +40,12 @@ def part_one():
 
 
 def contained_bags(bag: str) -> int:
-    return sum(n * contained_bags(t) for n, t in rules[bag]) + 1
+    return int(sum(n * contained_bags(t) for n, t in rules[bag]) + 1)
 
 
 def part_two():
     return contained_bags("shiny gold") - 1
 
 
-aoc_helper.lazy_submit(day=7, year=2020, solution=part_one)
-aoc_helper.lazy_submit(day=7, year=2020, solution=part_two)
+print(part_one())
+print(part_two())
