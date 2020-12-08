9d07a9ccb866de6138802df225fb842d64e58910
starwort <tcphone93@gmail.com>
2020-12-08 11:37:48 +0000

    Untested generator

--- ante++/day_07_gen.py
+++ ante++/day_07_gen.py
@@ -14,21 +14,37 @@ for _ in range(7):
 TOTAL_BAGS = len(ALL_BAGS)
 
 
+def format_bag_list(bags: list[tuple[float, str]]) -> str:
+    return ", ".join(f"{num} {kind} bag{'s' if num != 1 else ''}" for num, kind in bags)
+
+
 def format_rule(rule: tuple[str, list[tuple[float, str]]]) -> str:
     name, contains = rule
     if not contains:
         return f"{name} bags contain no other bags."
+    else:
+        return f"{name} bags contain {format_bag_list(contains)}."
 
 
 def random_amount() -> float:
     ipart = random.randrange(-5000, 5000)
-    fpart = random.random()
-    return ipart + fpart
+    fpart = int(random.random() * 4) / 4
+    return (ipart + (fpart or 0)) or 1
 
 
-def generate_input():
+def generate_input() -> str:
     used_bags = set()
-    rules = {}
+    rules_dict = {}
     for bag in ALL_BAGS:
         contains = random.randint(0, len(used_bags))
-        rules[bag] = [(random_amount(), i) for i in random.sample(used_bags, contains)]
+        rules_dict[bag] = [
+            (random_amount(), i) for i in random.sample(used_bags, contains)
+        ]
+        used_bags.add(bag)
+    rules = [format_rule(i) for i in rules_dict.items()]
+    for _ in range(7):
+        random.shuffle(rules)
+    return "\n".join(rules)
+
+
+(pathlib.Path(__file__).parent / "07.in").write_text(generate_input())
