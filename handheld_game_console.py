28f7f61c2b7808e12cea45124064bb88c304b57d
starwort <tcphone93@gmail.com>
2020-12-08 06:32:54 +0000

    Add argument parsing because pydis made me worry I needed to

--- handheld_game_console.py
+++ handheld_game_console.py
@@ -1,5 +1,13 @@
+class Arg:
+    def __init__(self, value: str) -> None:
+        self._value = value
+
+    def value(self, computer: "HandheldGameConsole") -> int:
+        return int(self._value)
+
+
 class HandheldGameConsole:
-    def __init__(self, instructions: list[tuple[str, int]]) -> None:
+    def __init__(self, instructions: list[tuple[str, list[Arg]]]) -> None:
         self.accumulator = 0
         self.ip = 0
         self.instructions = instructions
@@ -13,25 +21,27 @@ class HandheldGameConsole:
     def load_from_string(cls, instructions: str):
         return cls(
             [
-                (opc, int(opr))
-                for opc, opr in [line.split() for line in instructions.splitlines()]
+                (opc, list(map(Arg, opr.split(","))))
+                for opc, opr in [
+                    line.split(maxsplit=1) for line in instructions.splitlines()
+                ]
             ]
         )
 
-    def acc(self, operand: int) -> None:
-        self.accumulator += operand
+    def acc(self, operand: Arg) -> None:
+        self.accumulator += operand.value(self)
         self.ip += 1
 
-    def jmp(self, operand: int) -> None:
-        self.ip += operand
+    def jmp(self, operand: Arg) -> None:
+        self.ip += operand.value(self)
 
-    def nop(self, _: int) -> None:
+    def nop(self, _: Arg) -> None:
         self.ip += 1
 
     def step(self) -> None:
         opc, opr = self.instructions[self.ip]
         self.last_instruction = opc
-        getattr(self, opc)(opr)
+        getattr(self, opc)(*opr)
 
     def run_until(self, opcode: str) -> None:
         self.step()  # ensure that we run at least one instruction
