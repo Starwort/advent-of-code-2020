62ebf48b6a1e5399b7820f85fe9b06937c7041e5
starwort <tcphone93@gmail.com>
2020-12-25 05:13:01 +0000

    Day 25

--- day_25.py
+++ day_25.py
@@ -1,23 +1,35 @@
+from itertools import count
+
 import aoc_helper
 
 raw = aoc_helper.fetch(25, year=2020)
-print(raw)
+# print(raw)
 
 
 def parse_raw():
-    ...
+    return aoc_helper.extract_ints(raw)
 
 
-data = parse_raw()
+card_pky, door_pky = parse_raw()
+# card_pky, door_pky = [5764801, 17807724]
+
+
+def transform(subject, loop_size):
+    value = 1
+    for _ in range(loop_size):
+        value *= subject
+        value %= 20201227
+    return value
 
 
 def part_one():
-    ...
-
-
-def part_two():
-    ...
+    value = 1
+    for card_ls in count(start=1):
+        value *= 7
+        value %= 20201227
+        if value == card_pky:
+            break
+    return transform(door_pky, card_ls)
 
 
 aoc_helper.lazy_submit(day=25, year=2020, solution=part_one)
-aoc_helper.lazy_submit(day=25, year=2020, solution=part_two)
